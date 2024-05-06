e4495984b09e06cb7ce5ea15be41f6410e113697
Muntazir Mehdi <168342549+minde-786@users.noreply.github.com>
2024-05-07 00:42:52 +0500

    todos project use for add our daily routines

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+let toDos=[];
+let condition=true;
+while(condition)
+    
+{
+ let taskadd =await inquirer.prompt(
+    [
+        {
+        name:"todo",
+        type:"input",
+        message:"add in todos which you want",
+    
+     },
+    {
+        name:"addMore",
+        type:"confirm",
+        message:"do you want to add more",
+    default:false,
+     }]);
+    
+    
+ 
+
+   
+toDos.push(taskadd.todo)
+condition= taskadd.addMore;
+console.log(toDos)}
